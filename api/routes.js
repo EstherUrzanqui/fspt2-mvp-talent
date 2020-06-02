924e6c4258ed20260426a3ad6b4d61a802f961ab
Esther Urzanqui <estherurzanqui@Esthers-MBP.home>
2020-06-02 11:10:09 +0200

    six

--- api/routes.js
+++ api/routes.js
@@ -33,7 +33,14 @@ routes.get('/candidates/department/:department', (req, res) => {
 routes.post("/candidates/", (req, res) => {
   const { firstname, lastname, email_address, mother_tongue, department, experience, relocation, remote, company_id, title, description } = req.body;
   db(
-    `INSERT INTO candidates (firstname,lastname, email_address, mother_tongue, department, experience, relocation, remote, company_id) VALUES ('${firstname}', '${lastname}', '${email_address}', '${mother_tongue}', '${department}', '${experience}', '${relocation}', '${remote}', '${company_id}') SELECT LAST_INSERT_ID() INTO @candidatesId; INSERT INTO skills (title, description) VALUES ('${title}, '${description}') SELECT LAST_INSERT_ID() INTO @skilLsId; INSERT INTO candidates_skills (candidate_id, skills_id) VALUES (@candidatesId, @skillsId);`
+    `SET @candidatesId = "candidates.id;
+    SET @skillsId = "skills.id;
+    INSERT INTO candidates (firstname,lastname, email_address, mother_tongue, department, experience, relocation, remote, company_id) 
+    VALUES ('${firstname}', '${lastname}', '${email_address}', '${mother_tongue}', '${department}', '${experience}', '${relocation}', '${remote}', '${company_id}'); 
+    SELECT LAST_INSERT_ID() INTO @candidatesId; 
+    INSERT INTO skills (title, description) VALUES ('${title}, '${description}');
+    SELECT LAST_INSERT_ID() INTO @skilLsId; 
+    INSERT INTO candidates_skills (candidate_id, skills_id) VALUES (@candidatesId, @skillsId);`
   )
     .then(results => {
       if (!results.error) {
@@ -72,21 +79,39 @@ routes.get('/companies' , (req, res) => {
 routes.get('/companies/candidates/:company' , (req, res) => {
   const { company } = req.params;
 
-  db(`SELECT candidates.mother_tongue as mother_tongue, candidates.department as department, candidates.experience as experience, candidates.relocation as relocation, candidates.remote as remote, companies.name as company, companies.City as city FROM candidates INNER JOIN companies ON companies.id = candidates.company_id;`).then(results => {
+  db(`SELECT candidates.mother_tongue as mother_tongue, candidates.department as department, candidates.experience as experience, candidates.relocation as relocation, candidates.remote as remote, companies.name as company, companies.City as city 
+  FROM candidates 
+  INNER JOIN companies ON companies.id = candidates.company_id;`).then(results => {
       if(results.error) {
           res.status(400).send({ message: 'There was an error'})
       }
+
       
       res.send(results.data)
   })
 }) 
 
 routes.get('/candidates/skills' , (req, res) => {
-  db('SELECT candidates.mother_tongue, candidates.department, candidates.experience, candidates.relocation, candidates.remote, skills.title FROM candidates INNER JOIN candidates_skills ON candidates_skills.candidate_id = candidates.id INNER JOIN skills ON skills.id = candidates_skills.skills_id;') 
+  db(`SELECT candidates.mother_tongue, candidates.department, candidates.experience, candidates.relocation, candidates.remote, skills.title 
+  FROM candidates 
+  INNER JOIN candidates_skills ON candidates_skills.candidate_id = candidates.id 
+  INNER JOIN skills ON skills.id = candidates_skills.skills_id;`) 
     .then(results => {
-      if(results.error) {
-        res.status(400).send({ message: 'There was an error'})
-    }
+      const dataArray = [];
+
+      for(let i=0; i<results.length; i++) {
+        const dataItem = [];
+
+        dataItem.push(results[i]);
+
+        while(i < results.length-1 && results[i].id === results[i+1].id) {
+          i++
+          dataItem.push(results[i]);
+        }
+
+        dataArray.push(dataItem);
+      }
+      console.log('Final Array:', dataArray)
     
     res.send(results.data)
     })
